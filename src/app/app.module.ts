0f913a013a9038fa9a5a37faf2d33a51442083ae
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-04 15:52:06 +0100

    remove body tag from template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EquipmentsComponent } from './modules/equipments/components/equipments/equipments.component';
 import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
-  ],
+    SharedModule,
+    ],
   providers: [
     provideClientHydration()
   ],
