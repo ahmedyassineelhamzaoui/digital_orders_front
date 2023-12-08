9a0bea4e4613787d54d60ae0cc1f7119163f91d1
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-12-08 11:22:06 +0100

    create methods to check all checkboxs after the click in check box in thead

--- src/app/modules/devis/components/devis-modal/devis-modal.component.ts
+++ src/app/modules/devis/components/devis-modal/devis-modal.component.ts
@@ -8,13 +8,14 @@ import { DevisModule } from '../../devis.module';
   styleUrl: './devis-modal.component.scss'
 })
 export class DevisModalComponent {
-
+  demandes : any[] = [];
+  isLoading : boolean = false;
+  selectAll: boolean = false;
   constructor(
     public dialogRef:MatDialogRef<DevisModalComponent>,
     public devisService:DevisService
     ) { }
-    demandes : any[] = [];
-    isLoading : boolean = false;
+    
   closeModal(){
     this.dialogRef.close();
   }
@@ -24,12 +25,27 @@ export class DevisModalComponent {
   getAllDevis(){
     this.isLoading = true;
     return this.devisService.getAllDevis().subscribe((res:any)=>{
-      this.demandes = res; 
+      this.demandes = res.map((item: any) => ({ ...item, isSelected: false }));;
       this.isLoading = false;
       },
       (err:any)=>{
         console.log(err)
         this.isLoading = false;
       });
+  }
+  toggleSelectAll(){
+      this.demandes.forEach((demande:any)=>{
+        demande.isSelected = this.selectAll;
+      });
+  }
+  checkBoxChanged(){
+       if(this.isAllcheckBoxesSelected()){
+        this.selectAll = true;
+       }else{
+        this.selectAll = false;
+       }
+  }
+  isAllcheckBoxesSelected(){
+    return this.demandes.every(demande => demande.isSelected);
   }
 }
